9ae1d10097250cab3836b151c81c50934394ea65
karenwsit <karenwsit@gmail.com>
2016-02-06 15:22:36 -0800

    established OAuth Process

--- scripts/twitter_streaming.py
+++ scripts/twitter_streaming.py
@@ -3,7 +3,16 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream
 
 
-class MyStreamListener(StreamListener):
+# Twitter OAuth
+auth = OAuthHandler(os.environ.get('TWITTER_CONSUMER_KEY'), os.environ.get('TWITTER_CONSUMER_SECRET'))
+auth.set_access_token(os.environ.get('TWITTER_ACCESS_TOKEN'), os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
 
-    def on_status(self, status):
-        print(status.text)
+
+class myStreamListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+
+myStreamListener = myStreamListener()
+twitterStream = Stream(auth, listener=myStreamListener)
+twitterStream.filter(track=['python'])
